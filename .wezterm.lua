b102d01fcaacf8d68aea9e2e5ab2963525500237
Lysender <lysender@darkstar-win>
2024-02-23 23:12:10 +0800

    fix: add wezterm config

---
+++ .wezterm.lua
@@ -0,0 +1,15 @@
+-- Pull in the wezterm API
+local wezterm = require 'wezterm'
+
+-- This will hold the configuration.
+local config = wezterm.config_builder()
+
+-- This is where you actually apply your config choices
+
+-- For example, changing the color scheme:
+config.color_scheme = 'Abernathy'
+
+config.font = wezterm.font("MonoLisa", { weight="Regular", stretch="Normal", style="Normal" })
+
+-- and finally, return the configuration to wezterm
+return config
