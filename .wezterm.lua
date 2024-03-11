af7dd40ff04f658adbff03c0e76b6dead0b45e43
Lysender <lysender@darkstar-win>
2024-03-11 10:30:23 +0800

    fix: change theme to builtin dark

--- .wezterm.lua
+++ .wezterm.lua
@@ -1,5 +1,5 @@
 -- Pull in the wezterm API
-local wezterm = require 'wezterm'
+local wezterm = require("wezterm")
 
 -- This will hold the configuration.
 local config = wezterm.config_builder()
@@ -7,9 +7,9 @@ local config = wezterm.config_builder()
 -- This is where you actually apply your config choices
 
 -- For example, changing the color scheme:
-config.color_scheme = 'Abernathy'
+config.color_scheme = "Builtin Dark"
 
-config.font = wezterm.font("MonoLisa", { weight="Regular", stretch="Normal", style="Normal" })
+config.font = wezterm.font("MonoLisa", { weight = "Regular", stretch = "Normal", style = "Normal" })
 
 -- and finally, return the configuration to wezterm
 return config
