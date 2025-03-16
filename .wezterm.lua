50b490f5819a7e202abb1a268b4c6b5b48bbf059
Lysender <lysender@darkstar-win>
2025-03-16 21:34:05 +0800

    fix: powershell not working

--- .wezterm.lua
+++ .wezterm.lua
@@ -7,7 +7,6 @@ local act = wezterm.action
 local config = wezterm.config_builder()
 
 -- This is where you actually apply your config choices
--- config.default_prog = { "powershell.exe", "-NoLogo" }
 
 -- For example, changing the color scheme:
 -- "Builtin Dark"
