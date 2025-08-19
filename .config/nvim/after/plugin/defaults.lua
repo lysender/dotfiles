85acb04b4262db70598b138774dfa665a8567f23
Lysender <lysender@ubuntu-home-lab>
2025-08-19 07:31:04 +0000

    fix: minimal nvim config without lsp

---
+++ .config/nvim/after/plugin/defaults.lua
@@ -0,0 +1,21 @@
+vim.opt.number = true
+vim.opt.relativenumber = true
+
+-- Default tab settings
+-- Override tab settings by using .editorconfig
+vim.opt.tabstop = 4
+vim.opt.softtabstop = 4
+vim.opt.shiftwidth = 4
+
+vim.opt.expandtab = true
+vim.opt.autoindent = true
+vim.opt.smartindent = true
+
+vim.opt.wrap = false
+
+vim.opt.hlsearch = false
+vim.opt.incsearch = true
+
+vim.opt.scrolloff = 8
+vim.opt.signcolumn = "yes"
+vim.opt.colorcolumn = "120"
---
+++ .config/nvim/lua/custom/remap.lua
@@ -0,0 +1,25 @@
+-- Show netrw file explorer
+-- However, we got oil.nvim, so disable it for now
+-- vim.keymap.set("n", "<leader>pp", vim.cmd.Ex)
+
+-- Create vertical split window
+vim.keymap.set("n", "<leader>vv", ":vsplit<CR>")
+
+-- Create horizontal split window
+vim.keymap.set("n", "<leader>hh", ":split<CR>")
+
+-- Open terminal in a vertical split window
+vim.keymap.set("n", "<leader>tt", ":vsplit<CR>:term<CR>")
+
+-- Open terminal in a horizontal split window
+vim.keymap.set("n", "<leader>st", ":split<CR>:term<CR>")
+
+vim.keymap.set("n", "<leader>op", ":Copilot enable<CR>")
+vim.keymap.set("n", "<leader>oop", ":Copilot disable<CR>")
+
+-- Move highlighted block up or down
+vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
+vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
+
+-- Paste highlighted text without losing the previous text from clipboard
+vim.keymap.set("x", "<leader>p", [["_dP]])
